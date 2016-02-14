5f4c69214b105e8676bcc3b6e5d0754c6420001f
spoojar1 <surajenggp@gmail.com>
2016-02-13 21:27:21 -0600

    initial update

---
+++ HackerRank Code/Greedy/Easy/Jim and the Orders.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[][]=new int[n][4];
+		for(int i=0;i<n;i++){
+			a[i][0]=i;
+			a[i][1]=s.nextInt();
+			a[i][2]=s.nextInt();
+			a[i][3]=a[i][1]+a[i][2];
+		}
+		java.util.Arrays.sort(a,new java.util.Comparator<int[]>(){
+			public int compare(int a1[],int a2[]){
+				return Integer.compare(a1[3],a2[3]);
+			}
+		});
+		for(int i=0;i<n;i++)
+			System.out.print((a[i][0]+1)+" ");
+   }
+}
